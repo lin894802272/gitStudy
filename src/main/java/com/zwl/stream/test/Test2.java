847898b8a948123e0bd5b9ce5919152f55bc5f1b
weilin <894802272@qq.com>
2019-12-02 00:09:41 +0800

    我在master的提交

--- src/main/java/com/zwl/stream/test/Test2.java
+++ src/main/java/com/zwl/stream/test/Test2.java
@@ -7,5 +7,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("我这是在test1分支的代码");
         System.out.println("我是master分支输入的一句");
+
+        System.out.println("我在master的提交");
     }
 }
