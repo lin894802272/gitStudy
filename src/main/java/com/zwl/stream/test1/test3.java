d910bd38678945e68ec30b993c6df1433d196737
weilin <894802272@qq.com>
2019-12-01 23:56:15 +0800

    test3文件

---
+++ src/main/java/com/zwl/stream/test1/test3.java
@@ -0,0 +1,10 @@
+package com.zwl.stream.test1;
+
+/**
+ * Created by Administrator on 2019/12/1 0001.
+ */
+public class test3 {
+    public static void main(String[] args) {
+        System.out.println("这是继承test1的test2分支");
+    }
+}
